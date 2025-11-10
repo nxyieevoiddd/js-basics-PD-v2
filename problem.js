595b5420a05e500f95ad5934cb215352462e73b3
nxyieee <bindris.olivers@gmail.com>
2025-11-10 17:23:18 +0200

    6.uzd

--- problem.js
+++ problem.js
@@ -1,14 +1,30 @@
-let club =[
-    {vards : "Jānis", uzvards: "liepa", loma: "leader", hour:1},
-    {vards : "Janka", uzvards: "dzirnava", loma: 'leader', hour:3},
-    {vards : "Kevins", uzvards: "Gilovs", loma: member, hour:2},
-    {vards : "Sergejs", uzvards: "pods", loma: member, hour:1},
-    {vards : "Andrejs", uzvards: "brivna", loma: member, hour:7},
-    {vards : "Peters", uzvards: "merkurijs", loma: member, hour:3},
-    {vards : "Vitols", uzvards: "garāža", loma: member, hour:2},
-]
-for( i = "leader" ; i < club.loma; i++ ) {
-    club.loma ="leader";
-    console.log(i);
+let club = [
+  { vards: "Jānis", uzvards: "Liepa", loma: "leader", hour: 1 },
+  { vards: "Janka", uzvards: "Dzirnava", loma: "leader", hour: 3 },
+  { vards: "Kevins", uzvards: "Gilovs", loma: "member", hour: 2 },
+  { vards: "Sergejs", uzvards: "Pods", loma: "member", hour: 1 },
+  { vards: "Andrejs", uzvards: "Brīvna", loma: "member", hour: 7 },
+  { vards: "Pēteris", uzvards: "Merkurijs", loma: "member", hour: 3 },
+  { vards: "Vītols", uzvards: "Garāža", loma: "member", hour: 2 },
+];
+
+console.log("Vadītāji:");
+for (let i = 0; i < club.length; i++) {
+  if (club[i].loma === "leader") {
+    console.log(club[i].vards + " " + club[i].uzvards);
+  }
 }
-console.log("kopā ir 19 stundas")
+
+let totalHours = 0;
+for (let i = 0; i < club.length; i++) {
+  totalHours += club[i].hour;
+}
+console.log("Kopā ir " + totalHours + " brīvprātīgo stundas.");
+
+let memberHours = 0;
+for (let i = 0; i < club.length; i++) {
+  if (club[i].loma === "member") {
+    memberHours += club[i].hour;
+  }
+}
+console.log("Biedri kopā nostrādājuši " + memberHours + " stundas.");
