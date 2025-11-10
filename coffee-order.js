b90c97739f51d4ea2198efdae3ecf7d6f775db57
DeivisStasuls <deivis.stasuls@gmail.com>
2025-11-10 16:38:56 +0200

    3 uzd pabeigts

---
+++ coffee-order.js
@@ -0,0 +1,23 @@
+let coffeeSize   ;
+let addSugar = true;
+let small = 1;
+let medium =2;
+let large = 3;
+let smallS = 1.5;
+let mediumS =2.5;
+let largeS = 3.5;
+function testNum(coffeeSize = 1) {
+  let result;
+  if (3 < 1) {
+    result = "1€";
+  } else {
+    result = "1,5€";
+  }
+   if (3 < 2){
+    result= "1,5€" ;
+   } else {
+    result = "2,2€" ;
+   }
+    return result;
+}
+console.log(testNum(3));
