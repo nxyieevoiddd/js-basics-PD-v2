59e881001d0ee1096c98b842c948af2e2d846a53
DeivisStasuls <deivis.stasuls@gmail.com>
2025-11-10 17:03:41 +0200

    4.uzd nepabeigts

---
+++ books-array.js
@@ -0,0 +1,5 @@
+let invetory = [
+    {title: "Grega dienasgrāmata ", author: "deivs pilkejs", available: "nav"},
+    {title: "Mazais pūķis Kokosrieksts", author: "nezināms", available: "ir"},
+]
+let( i=0 , i<invetory.length , i++);
